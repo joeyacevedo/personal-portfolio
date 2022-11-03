50fea62ef25488ddffa7921de9e2124067abffe3
Joey Acevedo <thejosephacevedo@gmail.com>
2022-11-03 08:42:50 -0400

    work in progress

---
+++ postcss.confis.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+};
