bea7658f626a265145d7eaabe562b7b3da76d946
Joey Acevedo <thejosephacevedo@gmail.com>
2022-11-03 18:11:53 -0400

    commenting out pages/api.js because of build issue

--- pages/api.js
+++ pages/api.js
@@ -1,10 +1,10 @@
-// req = HTTP incoming message, res = HTTP server response
-export default function handler(req, res) {
-  // ...
-}
+// // req = HTTP incoming message, res = HTTP server response
+// export default function handler(req, res) {
+//   // ...
+// }
 
-//Potential route to hit for handling form inputs that need to be saved to a database
-export function handler(req, res) {
-  const email = req.body.email;
-  // Then save email to your database, etc...
-}
+// //Potential route to hit for handling form inputs that need to be saved to a database
+// export function handler(req, res) {
+//   const email = req.body.email;
+//   // Then save email to your database, etc...
+// }
